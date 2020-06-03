4853c762aaba27ba187bb4f848d28f98a335f4cb
Leenert Specht <leenert.specht@gmail.com>
2020-06-03 18:06:38 +0200

    FIXED Minor stuff INTEGRATED Authentication State

--- app/components/base/navigation.js
+++ app/components/base/navigation.js
@@ -3,30 +3,38 @@ import {Col, Row, Button, Layout, Menu, Space} from "antd";
 import {MenuOutlined} from '@ant-design/icons';
 import MachbarschaftLogo from "../../assets/img/logo/machbarschaft-logo.png";
 import {NavLink} from "react-router-dom";
+import useAuthentication from "../../hooks/useAuthentication";
 
-const { Header, Content, Footer } = Layout;
+const {Header, Content, Footer} = Layout;
 
-function NavigationMenuDefault(props) {
-    return (
-        <Menu mode={props.mode} defaultSelectedKeys={['1']} onClick={props.menuClicked}>
-            <Menu.Item key="1"><NavLink to={"/"} exact={true}>DAS PROJEKT</NavLink></Menu.Item>
-            <Menu.Item key="2"><NavLink to={"/"} exact={true}>SO FUNKTIONIERT'S</NavLink></Menu.Item>
-            <Menu.Item key="3"><NavLink to={"/"} exact={true}>ÜBER UNS</NavLink></Menu.Item>
-            <Menu.Item key="4"><NavLink to={"/"} exact={true}>FAQ</NavLink></Menu.Item>
-            <Menu.Item key="5"><NavLink to={"/"} exact={true}>KONTAKT</NavLink></Menu.Item>
-            <Menu.Item key="6"><NavLink to={"/"} exact={true}>DOWNLOAD APP</NavLink></Menu.Item>
-            <Menu.Item key="7"><NavLink to={"/"} exact={true}>ALS HELFER REGISTRIEREN</NavLink></Menu.Item>
-        </Menu>
-    );
-}
-function NavigationMenuLoggedIn(props) {
-    return (
-        <Menu mode={props.mode} defaultSelectedKeys={['1']} onClick={props.menuClicked}>
-            <Menu.Item key="1"><NavLink to={"/"} exact={true}>DASHBOARD</NavLink></Menu.Item>
-            <Menu.Item key="2"><NavLink to={"/"} exact={true}>AUFTRAG AUFGEBEN</NavLink></Menu.Item>
-            <Menu.Item key="3"><NavLink to={"/"} exact={true}>BRAUCHEN SIE HILFE?</NavLink></Menu.Item>
-        </Menu>
-    );
+function NavigationMenu(props) {
+    const {authenticationState} = props
+
+    if (authenticationState.user_id == null) {
+        // No User
+        return (
+            <Menu mode={props.mode} defaultSelectedKeys={['1']} onClick={props.menuClicked}>
+                <Menu.Item key="1"><NavLink to={"/"} exact={true}>DAS PROJEKT</NavLink></Menu.Item>
+                <Menu.Item key="2"><NavLink to={"/"} exact={true}>SO FUNKTIONIERT'S</NavLink></Menu.Item>
+                <Menu.Item key="3"><NavLink to={"/"} exact={true}>ÜBER UNS</NavLink></Menu.Item>
+                <Menu.Item key="4"><NavLink to={"/"} exact={true}>FAQ</NavLink></Menu.Item>
+                <Menu.Item key="5"><NavLink to={"/"} exact={true}>KONTAKT</NavLink></Menu.Item>
+                <Menu.Item key="6"><NavLink to={"/"} exact={true}>DOWNLOAD APP</NavLink></Menu.Item>
+                <Menu.Item key="7"><NavLink to={"/"} exact={true}>ALS HELFER REGISTRIEREN</NavLink></Menu.Item>
+            </Menu>
+        );
+    } else {
+        // User
+        return (
+            <Menu mode={props.mode} defaultSelectedKeys={['1']} onClick={props.menuClicked}>
+                <Menu.Item key="1"><NavLink to={"/dashboard"} exact={true}>DASHBOARD</NavLink></Menu.Item>
+                <Menu.Item key="2"><NavLink to={"/"} exact={true}>AUFTRAG AUFGEBEN</NavLink></Menu.Item>
+                <Menu.Item key="3"><NavLink to={"/"} exact={true}>BRAUCHEN SIE HILFE?</NavLink></Menu.Item>
+            </Menu>
+        );
+    }
+
+
 }
 
 function NavigationProfileIndicator() {
@@ -40,19 +48,24 @@ function NavigationProfileIndicator() {
 export default function Navigation() {
     const [mobileNavState, setState] = React.useState(false);
 
+    const [authenticationState, {
+        checkAuthentication,
+        invalidateAuthentication
+    }] = useAuthentication()
+
     return (
         <>
             <div className="nav-bar">
-                <MenuOutlined className="nav-menu-mobile-icon" onClick={() => setState(!mobileNavState)} />
-                <img className="nav-logo" src={MachbarschaftLogo} />
+                <MenuOutlined className="nav-menu-mobile-icon" onClick={() => setState(!mobileNavState)}/>
+                <img className="nav-logo" src={MachbarschaftLogo}/>
                 <div className="nav-menu-desktop">
-                    <NavigationMenuDefault mode="horizontal" menuClicked={() => setState(false)} />
+                    <NavigationMenu mode="horizontal" menuClicked={() => setState(false)} authenticationState={authenticationState}/>
                 </div>
-                <NavigationProfileIndicator />
+                <NavigationProfileIndicator/>
             </div>
             <div className="nav-menu-mobile">
                 <div className={"nav-menu-mobile-content " + (mobileNavState ? "nav-menu-mobile-content-open" : "nav-menu-mobile-content-close")}>
-                    <NavigationMenuDefault mode="vertical" menuClicked={() => setState(false)} />
+                    <NavigationMenu mode="vertical" menuClicked={() => setState(false)} authenticationState={authenticationState}/>
                 </div>
             </div>
         </>
--- app/index.js
+++ app/index.js
@@ -4,26 +4,16 @@ import './index.css'
 import "antd/dist/antd.less";
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import {Typography, Layout, Menu, Space} from 'antd';
+import {Typography, Layout} from 'antd';
 
 import Navigation from "./components/base/navigation";
 import Footer from "./components/base/footer";
 
-const {Title} = Typography;
-const {Header, Content} = Layout;
-
 const LandingPage = React.lazy(() => import("./components/landingPage/landingPage"))
 const Dashboard = React.lazy(() => import("./components/dashboard/dashboard"))
-
-//import Examples from "./components/examples/examples";
+const Examples = React.lazy(() => import("./components/examples/examples"))
 
 function App() {
-    // for viewing examples:
-    /*
-        <React.Fragment>
-            <Examples />
-        </React.Fragment>
-    */
     return (
         <Router>
             <Layout>
@@ -31,15 +21,14 @@ function App() {
 
                 <div className="site-layout">
                     <div className="main-content">
-
                         <React.Suspense fallback={<p>Lädt...</p>}>
                             <Switch>
                                 <Route exact path='/' component={LandingPage}/>
                                 <Route exact path='/dashboard' component={Dashboard}/>
+                                <Route exact path='/examples' component={Examples}/>
                                 <Route render={() => <h1>404</h1>}/>
                             </Switch>
                         </React.Suspense>
-
                     </div>
 
                     <Footer/>
