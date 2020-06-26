e5886105070c8b2225e63bfc26ea5af7c0cfafe3
Leenert Specht <leenert.specht@gmail.com>
2020-06-26 18:17:42 +0200

    FIXED Minor Stuff

--- app/utils/routing/routes-component.js
+++ app/utils/routing/routes-component.js
@@ -10,18 +10,16 @@ const ResetPassword = React.lazy(() => import("../../components/resetPassword/re
 const PlaceRequest = React.lazy(() => import("../../components/seekHelp/place-request"))
 
 export default function RoutesComponent() {
-    const authenticationContext = React.useContext(AuthenticationContext);
-
     return (
         <Switch>
             <Route exact path='/' component={LandingPage}/>
-            <Route path='/dashboard' component={Dashboard}/>
+            <Route path='/dashboard' render={() => <RouteAuthenticated component={Dashboard} redirectTo={"/login"}/>}/>
             <Route path='/examples' component={Examples}/>
             <Route path='/login' component={Login}/>
             <Route path='/resetpassword' component={ResetPassword}/>
             <Route path='/place-request' render={() => <RouteAuthenticated component={PlaceRequest} redirectTo={"/login"}/>}/>
 
-            <Route render={(props) => <h1>404</h1>}/>
+            <Route render={() => <h1>404</h1>}/>
         </Switch>
     )
 }
