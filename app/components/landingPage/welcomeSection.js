82a996b69784df6a00766019ae228d5aa4ff0260
Leenert Specht <leenert.specht@gmail.com>
2020-07-10 20:15:04 +0200

    FIXED Routing Error

--- app/components/landingPage/welcomeSection.js
+++ app/components/landingPage/welcomeSection.js
@@ -37,9 +37,7 @@ export default function WelcomeSection() {
         ? `/place-request?phoneNumber=${parsedPhoneNumber}`
         : '/place-request';
 
-    history.push({
-      pathname: redirectURL,
-    });
+    history.push(redirectURL);
   };
 
   return (
