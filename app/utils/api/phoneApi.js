2575b6e0076ec52291cf7a387663435835ebf7ec
Andreas Reinecke <andreas.reinecke@tum.de>
2020-07-15 17:54:35 +0200

    Changed TAN endpoint

--- app/utils/api/phoneApi.js
+++ app/utils/api/phoneApi.js
@@ -7,7 +7,7 @@ import { objectToFormUrlEncoded } from './formUrlEncoder';
  * @returns {Promise<Response>}
  */
 export const putConfirmTan = async (formValues) => {
-  const endpoint = `${apiUrl()}phone`;
+  const endpoint = `${apiUrl()}phone/tan`;
 
   const formBody = objectToFormUrlEncoded(formValues);
 
@@ -42,7 +42,7 @@ export const putConfirmTan = async (formValues) => {
  * @returns {Promise<Response>}
  */
 export const postRequestTan = async (formValues) => {
-  const endpoint = `${apiUrl()}phone`;
+  const endpoint = `${apiUrl()}phone/tan`;
 
   let formBody = objectToFormUrlEncoded(formValues);
 
