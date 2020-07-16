fbc141463697e4bfe5cc7fa880a5b7ced127ab28
Janik Müller <mueller@cqse.eu>
2020-07-16 15:01:25 +0200

    MB-54 create component

--- app/components/register/password-generation-questions.js
+++ app/components/register/password-generation-questions.js
@@ -1,92 +1,160 @@
 import React from 'react'
-import {useForm} from "react-hook-form";
-import {Form, Input, Radio, Typography} from 'antd';
-import * as yup from "yup";
-import PlaceRequestWizardValidationError from "../seekHelp/wizard/place-request-wizard-validation-error";
-import PlaceRequestWizardNavigation from "../seekHelp/wizard/place-request-wizard-navigation";
+import {Form, Input, Typography, Button} from 'antd';
+import AuthenticationContext from "../../contexts/authentication";
+import {useHistory} from "react-router-dom";
+import PropTypes from "prop-types";
 
 const {Title} = Typography
 
-function PasswordGenerationQuestions() {
-    const specialCharacters = ['!', ',', '.', '#', ';', '-', '_', '$', '%', '&', '+'];
+export default function PasswordGenerationQuestions({phone, forename, surname}) {
+    const authenticationContext = React.useContext(AuthenticationContext);
+    const history = useHistory();
 
-    const onSubmit = (data) => {
-        let password = data.input1 + data.input2 + data.input3;
-        if(password.toString().length ) {}
+    const specialCharacters = ['!', ',', '.', '#', ';', '-', '_', '$', '%', '&', '+'];
+    const questions = ['Wo sind Sie geboren?', 'Was ist Ihr Hobby?',
+        'Wie lautet der Nachname Ihres ersten Lehrers/Ihrer ersten Lehrerin?',
+        'Wie viele Geschwister hat Ihr Vater?', 'Wie viele Cousins und Cousinen haben Sie?'
+    ];
+
+    const generatePassword = (data) => {
+        let password = data.answer1.toString() + data.answer2.toString() + data.answer3.toString()
+            + specialCharacters[getRandomInt(specialCharacters.length)]
+            + specialCharacters[getRandomInt(specialCharacters.length)];
+        password = password.replace(/\s/g, ''); //remove whitespace
+        while(password.length < 12) {
+            password += specialCharacters[getRandomInt(specialCharacters.length)];
+        }
+        setPasswordState(password);
     }
+
+    function getRandomInt(max){
+        return Math.floor(Math.random() * max);
+    }
+
+    function pickQuestionsRandomly() {
+        form.resetFields(['answer1','answer2','answer3']);
+        let newSetQuestions = [];
+        while(newSetQuestions.length < 3){
+            const currentQuestion = questions[getRandomInt(5)];
+            if(!newSetQuestions.includes(currentQuestion)) newSetQuestions.push(currentQuestion);
+        }
+        setCurrentQuestionsState(newSetQuestions);
+    }
+
+    const handleForm = async (email, password) => {
+        const registerResult = await authenticationContext.performRegister(
+            email,
+            phone,
+            password,
+            forename,
+            surname
+        );
+        if (registerResult === true) {
+            history.push('/');
+        }
+    };
 
     const formLayout = {
         labelCol: {span: 8},
         wrapperCol: {span: 12},
     };
 
-    const [form] = Form.useForm();
-    const formName = "register-after-request";
+    const [showFormState, setShowFormState] = React.useState('start');
+    const [emailState, setEmailState] = React.useState('');
+    const [passwordState, setPasswordState] = React.useState('');
+    const [currentQuestionsState, setCurrentQuestionsState] = React.useState(['','','']);
 
-    /* As we are using Ant-Design, we need to register form fields manually during first build of a component. This can be done in a useEffect hook. */
-    React.useEffect(() => {
-        register({name: "input1"})
-        register({name: "input2"})
-        register({name: "input3"})
-    }, [])
+    const [form] = Form.useForm();
+    const formNameEmail = "email-form";
+    const formNamePassword = "password-questions-form";
 
     return (
         <>
-            <Title level={3}>Form Validation</Title>
+            <Title level={3}>Jetzt registrieren</Title>
+            {showFormState === 'start' && <Button type='primary' onClick={() => {setShowFormState('email')}}>Start</Button>}
 
-            <Form
+            {showFormState === 'email' && <>
+                <Form
+                    {...formLayout}
+                    form={form}
+                    name={formNameEmail}
+                    hideRequiredMark={true}
+                    onFinish={(formValues) => {
+                        setEmailState(formValues.email);
+                        pickQuestionsRandomly();
+                        setShowFormState('questions');
+                    }}
+                >
+                    <Form.Item label={"Bitte geben Sie ihre Email-Adresse an:"} name={"email"} rules={[
+                        {
+                            type: "email",
+                            required: true,
+                            message: "Sie müssen Ihre Email-Adresse angeben."
+                        }
+                    ]}>
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type='primary' htmlType='submit'>Weiter</Button>
+                    </Form.Item>
+                </Form>
+                </>}
+
+            {showFormState === 'questions' &&
+                <Form
                 {...formLayout}
                 form={form}
-                name={formName}
+                name={formNamePassword}
                 hideRequiredMark={true}
-                onFinish={(formValues) => handleNextPage(formName, formValues)}
-                initialValues={typeof wizardState.formData[formName] != 'undefined' ? {
-                    requestType: wizardState.formData[formName]["requestType"],
-                    carNecessary: wizardState.formData[formName]["carNecessary"],
-                    prescriptionRequired: wizardState.formData[formName]["prescriptionRequired"],
-                } : {}}>
-                <Form.Item label={"Um welche Art von Einkauf geht es?"} name={"requestType"} rules={[
+                onFinish={(formValues) => {generatePassword(formValues); setShowFormState('summary');}}
+                >
+                <Form.Item label={currentQuestionsState[0]} name={"answer1"} rules={[
                     {
                         required: true,
-                        message: "Bitte geben Sie an, um welche Art Einkauf es geht."
+                        message: "Bitte beantworten Sie die Frage."
                     }
                 ]}>
-                    <Radio.Group size={"large"}>
-                        <Radio.Button value={"groceries"}>Lebensmittel</Radio.Button>
-                        <Radio.Button value={"medication"}>Medikamente</Radio.Button>
-                        <Radio.Button value={"other"}>Anderes</Radio.Button>
-                    </Radio.Group>
+                    <Input/>
                 </Form.Item>
 
-                <Form.Item label={"Wird für die Einkäufe ein Auto benötigt?"} name={"carNecessary"} rules={[
+                <Form.Item label={currentQuestionsState[1]} name={"answer2"} rules={[
                     {
                         required: true,
-                        message: "Bitte geben Sie an, ob ein Auto benötigt wird."
+                        message: "Bitte beantworten Sie die Frage."
                     }
                 ]}>
-                    <Radio.Group size={"large"}>
-                        <Radio.Button value={"true"}>Ja</Radio.Button>
-                        <Radio.Button value={"false"}>Nein</Radio.Button>
-                    </Radio.Group>
+                    <Input/>
                 </Form.Item>
 
-                <Form.Item label={"Wird ein ärztliches Rezept benötigt?"} name={"prescriptionRequired"} rules={[
+                <Form.Item label={currentQuestionsState[2]} name={"answer3"} rules={[
                     {
                         required: true,
-                        message: "Bitte geben Sie an, ob ein Rezept benötigt wird."
+                        message: "Bitte beantworten Sie die Frage."
                     }
                 ]}>
-                    <Radio.Group size={"large"}>
-                        <Radio.Button value={"true"}>Ja</Radio.Button>
-                        <Radio.Button value={"false"}>Nein</Radio.Button>
-                    </Radio.Group>
+                    <Input/>
                 </Form.Item>
+                    <Form.Item>
+                        <Button type='primary' onClick={()=>setShowFormState('email')}>Zurück</Button>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type='primary' htmlType='submit'>Weiter</Button>
+                    </Form.Item>
+            </Form>}
 
-                {wizardState.hasError && <PlaceRequestWizardValidationError wizardState={wizardState}/>}
-
-                <PlaceRequestWizardNavigation handlePreviousPage={handlePreviousPage} wizardState={wizardState}/>
-            </Form>
+            {showFormState === 'summary' && <>
+                Ihr Passwort: {passwordState} <br/> Bitte notieren Sie sich ihr Passwort.<br/>
+                <Button type='primary' onClick={() => {
+                    pickQuestionsRandomly();
+                    setShowFormState('questions');}}>Zurück</Button>
+                <Button type='primary' onClick={() => handleForm(emailState, passwordState)}>Abschicken</Button></>}
         </>
 
     )
 }
+
+PasswordGenerationQuestions.propTypes = {
+    phone: PropTypes.string.isRequired,
+    forename: PropTypes.string.isRequired,
+    surname: PropTypes.string.isRequired,
+};
