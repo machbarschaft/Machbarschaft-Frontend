3acf907fda29cfe7de1ccc5f3f017a1ffe7d9873
Andreas Reinecke <andreas.reinecke@tum.de>
2020-07-19 21:39:47 +0200

    Landingpage, faq, imprint, privacy notice

--- app/components/landingPage/landingPage.js
+++ app/components/landingPage/landingPage.js
@@ -1,9 +1,69 @@
 import React from 'react';
 import WelcomeSection from './welcomeSection.js';
 import AuthenticationContext from '../../contexts/authentication';
+import WirVsVirus from '../../assets/img/landingpage/wirvsvirus_logo.jpg';
+import GrafikFive from '../../assets/img/landingpage/Grafik_5.png';
+import VerhaltensEmpfehlungenPdf from '../../assets/documents/Verhaltensempfehlungen_für_MACHBAR_EINKAUF.pdf';
+import FlyerPdf from '../../assets/documents/Flyer_für_Hilfesuchende.pdf';
+import { Typography } from 'antd';
+const { Title } = Typography;
 
 export default function LandingPage() {
   const authProps = React.useContext(AuthenticationContext);
 
-  return <WelcomeSection />;
+  return (
+    <div className={'overflow-scroll'}>
+      <WelcomeSection />
+      <div className={'landing-page-container'}>
+        <div className={'landingpage-spacing'}></div>
+        <div className={'landingpage-element'}>
+          <img src={WirVsVirus} className={'landingpage-wirvsvirus'} />
+        </div>
+        <div className={'landingpage-text landingpage-element'}>
+          Wir sind 1 von 20 ausgewählten Projekten aus insgesamt 1.500 eingereichten
+          Projekten des #WirVsVirus Hackathons der Bundesregierung und arbeiten an der Umsetzung.
+        </div>
+        <div className={'landingpage-divider landingpage-element'}></div>
+        <Title level={2} className={'landingpage-element landingpage-text text-align-center'}>
+          Mach mit uns deine Nachbarschaften zu Machbarschaften.
+        </Title>
+        <div className={'landingpage-two-section landingpage-element landingpage-text'}>
+          <img src={GrafikFive} className={'landingpage-two-section-image'} />
+          <div className={'landingpage-two-section-text'}>Deine Daten werden an unsere Datenbank
+          übertragen und dein Auftrag kommt bereits real in unserer App an. Wird dein Auftrag nicht
+          angenommen, erhältst du nach 24 Stunden einen Anruf, dass dein Auftrag noch nicht bearbeitet
+          werden konnte, du aber den Auftrag verlängern kannst. Beide Sequenzen kannst du testen, indem
+          du am Anfang des Anrufes zwischen beiden auswählen kannst (damit du nicht 24 Stunden auf
+          deinen Rückruf warten musst).</div>
+        </div>
+        <Title level={1} className={'landingpage-element landingpage-text text-align-center headline-red'}>
+          Ältere Menschen sind aktuell besonders gefährdet und hilfebedürftig, haben oft jedoch
+          keinen Zugang zu digitalen Nachbarschaftshilfen.
+        </Title>
+        <div className={'landingpage-text landingpage-element text-align-center'}>
+          Ein Anruf bei Machbarschaft hilft bereits.
+        </div>
+        <div className={'landingpage-two-section landingpage-element landingpage-text'}>
+          <div className={'landingpage-two-section-text'}>
+            <a href={VerhaltensEmpfehlungenPdf} target={'_blank'} className={'landingpage-download-button'}>
+              Empfehlung herunterladen
+            </a><br/><br/>
+            Unsere Hilfesuchenden tätigen einen einfachen Telefonanruf über unsere kostenfreie
+            Hotline und Ihre Anliegen werden über Systeme künstlicher Intelligenz verarbeitet und
+            schließlich an unsere MACHBAR:INNEN als Auftrag auf der Webapp oder in der mobilen App
+            angezeigt. Ganz ohne persönlichen Kontakt!
+          </div>
+          <div className={'landingpage-two-section-text'}>
+            <a href={FlyerPdf} target={'_blank'} className={'landingpage-download-button'}>
+              Flyer herunterladen
+            </a><br/><br/>
+            Damit du als MACHBAR:IN und Helfer:in bestmöglichvor Infektion geschützt bist,
+            haben wir bereits detaillierte Verhaltensregeln, die den RKI-Vorgaben entsprechen, für
+            dich und unsere Hilfesuchenden entwickelt. So sind alle geschützt!
+          </div>
+        </div>
+        <br/><br/><br/>
+      </div>
+    </div>
+  );
 }
